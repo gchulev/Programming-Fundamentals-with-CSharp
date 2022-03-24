22ced91d88847d02eba1b589629066205d97a7f5
gchulev <georgi.chulev@gmail.com>
2022-03-24 19:22:57 +0200

    Finally the Rexex worked

---
+++ 06. Extract Emails/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<=\s)([A-Za-z]+\.?-?_?\w+)@{1}(\w+-?\w+\.\w+\.?\w+)";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            Console.WriteLine(string.Join($"{ Environment.NewLine}", matches));
+        }
+    }
+}
