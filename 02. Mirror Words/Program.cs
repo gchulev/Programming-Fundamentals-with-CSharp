68c13a20d1658d5f7f04a2a15cac26142d3a00fb
gchulev <georgi.chulev@gmail.com>
2022-03-24 22:13:17 +0200

    Mirror Words task

--- 02. Mirror Words/Program.cs
+++ 02. Mirror Words/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _02._Mirror_Words
 {
@@ -7,10 +9,41 @@ namespace _02._Mirror_Words
     {
         static void Main()
         {
-            string test = "@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r";
+            string input = Console.ReadLine();
+            string pattern = @"(@|#)(?<firstword>[A-Za-z]{3,})(\1)(\1)(?<secondword>[A-Za-z]{3,})(\1)?";
+            MatchCollection matches = Regex.Matches(input, pattern);
 
-            var result = test.Split(new char[] { '#', '@' },StringSplitOptions.RemoveEmptyEntries);
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+                List<string> mirrorWords = new List<string>();
 
+                foreach (Match match in matches)
+                {
+                    string firstWord = match.Groups["firstword"].Value;
+                    string secondWord = match.Groups["secondword"].Value;
+                    string reversedSecondWord = string.Join("", secondWord.Reverse());
+
+                    if (firstWord == reversedSecondWord)
+                    {
+                        mirrorWords.Add($"{firstWord} <=> {secondWord}");
+                    }
+                }
+                if (mirrorWords.Count > 0)
+                {
+                    Console.WriteLine("The mirror words are:");
+                    Console.WriteLine(string.Join(", ", mirrorWords));
+                }
+                else
+                {
+                    Console.WriteLine("No mirror words!");
+                }
+            }
         }
     }
 }
