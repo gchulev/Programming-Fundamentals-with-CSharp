ce636b94ce0710da13b2dd6a30b5ad681b81c824
gchulev <georgi.chulev@gmail.com>
2022-03-26 10:55:49 +0200

    Added SoftUni Problem 'Destination Mapper'

---
+++ 02. Destination Mapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            List<string> destinations = new List<string>();
+            Regex rgx = new Regex(@"(=|\/)(?<location>[A-Z][A-Za-z]{3,})\1");
+            MatchCollection matches = rgx.Matches(input);
+            int travelPoints = 0;
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    string location = match.Groups["location"].Value;
+                    destinations.Add(location);
+                    travelPoints += location.Length;
+                }
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
