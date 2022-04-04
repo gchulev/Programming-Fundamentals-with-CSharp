4f3e8e7e42f1e5b5b91301b598a55ac5a80d6a18
gchulev <georgi.chulev@gmail.com>
2022-04-04 16:44:50 +0300

    Added SoftUni problem 'Treasure Finder'

---
+++ 03. Treasure Finder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _03._Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] key = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            while (input != "find")
+            {
+                int iteration = 1;
+                int keyIndex = -1;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i >= iteration * key.Length)
+                    {
+                        keyIndex = i - iteration * key.Length;
+                        iteration++;
+                    }
+                    else
+                    {
+                        keyIndex++;
+                    }
+                    sb.Append((char)(input[i] - key[keyIndex]));
+                }
+                var test = sb.ToString().Split('&');
+                var test2 = sb.ToString().Split(new char[] { '<', '>' });
+                string treasureType = string.Join(string.Empty, sb.ToString().Split('&')[1]);
+                string coordinates = string.Join(string.Empty, sb.ToString().Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries)[1]);
+                Console.WriteLine($"Found {treasureType} at {coordinates}");
+                sb.Clear();
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
