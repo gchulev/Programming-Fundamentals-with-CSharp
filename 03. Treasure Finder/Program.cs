7b676b2907a9c0fd969c89ac9b7cd256f32ec981
gchulev <georgi.chulev@gmail.com>
2022-04-04 17:21:29 +0300

    Saving last changes

--- 03. Treasure Finder/Program.cs
+++ 03. Treasure Finder/Program.cs
@@ -29,11 +29,12 @@ namespace _03._Treasure_Finder
                     }
                     sb.Append((char)(input[i] - key[keyIndex]));
                 }
-                var test = sb.ToString().Split('&');
-                var test2 = sb.ToString().Split(new char[] { '<', '>' });
+                
                 string treasureType = string.Join(string.Empty, sb.ToString().Split('&')[1]);
                 string coordinates = string.Join(string.Empty, sb.ToString().Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries)[1]);
+
                 Console.WriteLine($"Found {treasureType} at {coordinates}");
+
                 sb.Clear();
                 input = Console.ReadLine();
             }
